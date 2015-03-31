a2f07eb91e749a3859e6208f1eb638985443599e
Dan Liew <daniel.liew@imperial.ac.uk>
2015-03-31 16:58:30 +0100

    Make GPUVerifyRise4Fun the default container entry point and modify instructions to reflect this and also state how shell access can be gained.

--- gpuverify-rise4fun-config.py
+++ gpuverify-rise4fun-config.py
@@ -8,7 +8,7 @@ SRC_ROOT = os.path.abspath(os.path.dirname(__file__)) # Find where this file liv
 GPUVERIFY_ROOT_DIR= '/home/gv/gpuverify' # Development or deploy root directory for GPUVerify
 GPUVERIFY_TEMP_DIR=None # The directory to place temporary files during GPUVerify execution. None will use system default.
 GPUVERIFY_TIMEOUT=30 # Number of seconds to wait for result before giving up
-KERNEL_COUNTER_PATH= os.getcwd() # This sets the directory to place KernelCounterObserver pickle files
+KERNEL_COUNTER_PATH= '/data/' # This sets the directory to place KernelCounterObserver pickle files
 
 # If set true the version number will include the output of running --version on GPUVerify in this repository.
 # You should set this to False if you have not configured the local version of GPUVerify
