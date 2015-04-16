3c9851e74dc0a6bd512f49dd4ab25e49822e04ba
Dan Liew <daniel.liew@imperial.ac.uk>
2015-04-16 15:49:56 +0100

    Structure logged data into the volume and also log submitted kernels.

--- gpuverify-rise4fun-config.py
+++ gpuverify-rise4fun-config.py
@@ -8,7 +8,7 @@ SRC_ROOT = os.path.abspath(os.path.dirname(__file__)) # Find where this file liv
 GPUVERIFY_ROOT_DIR= '/home/gv/gpuverify' # Development or deploy root directory for GPUVerify
 GPUVERIFY_TEMP_DIR=None # The directory to place temporary files during GPUVerify execution. None will use system default.
 GPUVERIFY_TIMEOUT=30 # Number of seconds to wait for result before giving up
-KERNEL_COUNTER_PATH= '/data/' # This sets the directory to place KernelCounterObserver pickle files
+KERNEL_COUNTER_PATH= '/data/counters' # This sets the directory to place KernelCounterObserver pickle files
 
 # If set true the version number will include the output of running --version on GPUVerify in this repository.
 # You should set this to False if you have not configured the local version of GPUVerify
@@ -21,4 +21,4 @@ INCLUDE_VERSION_FROM_LOCAL=True
 GPUVERIFY_DEFAULT_ARGS= ['--verbose']
 
 # The directory to log recieved kernels. Set to None to disable
-LOGGED_KERNELS_DIR = None
+LOGGED_KERNELS_DIR = '/data/logged_kernels'
